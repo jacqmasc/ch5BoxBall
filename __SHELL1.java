0e209016e0cb3e5bc3a16fcab259a042a8657155
Jackie <andianyue@yahoo.com>
2015-04-20 11:05:17 -0400

    More changes
    Mostly with color

---
+++ __SHELL1.java
@@ -0,0 +1,10 @@
+
+public class __SHELL1 extends bluej.runtime.Shell {
+public static void run() throws Throwable {
+final bluej.runtime.BJMap __bluej_runtime_scope = getScope("C:\\Users\\G00181342\\Desktop\\ch5BoxBall");
+final BallDemo ballDemo1 = (BallDemo)__bluej_runtime_scope.get("ballDemo1");
+
+
+ballDemo1.boxBounce();
+
+}}
